e535b91f4d1c681195dbfba3f4dec317429a19e5
Yashch917 <120004140+Yashch917@users.noreply.github.com>
2025-07-28 09:28:55 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,44 @@
+package lab2;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        System.out.print("Enter two integers to add: ");
+        int int1 = scanner.nextInt();
+        int int2 = scanner.nextInt();
+        int result1 = calculator.add(int1, int2);
+        System.out.println("Sum of two integers: " + result1);
+
+        System.out.print("Enter three integers to add: ");
+        int int3 = scanner.nextInt();
+        int int4 = scanner.nextInt();
+        int int5 = scanner.nextInt();
+        int result2 = calculator.add(int3, int4, int5);
+        System.out.println("Sum of three integers: " + result2);
+
+        System.out.print("Enter two decimal numbers to add: ");
+        double double1 = scanner.nextDouble();
+        double double2 = scanner.nextDouble();
+        double result3 = calculator.add(double1, double2);
+        System.out.println("Sum of two doubles: " + result3);
+
+        scanner.close();
+    }
+}
